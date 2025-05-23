45316b1bb368b5975a0842be2def2f7404e319b6
PrpVirusM <danik4814let@gmail.com>
2025-05-23 23:53:06 +0300

    createorders

--- backend/app/src/Controller/OrdersStacksController.php
+++ backend/app/src/Controller/OrdersStacksController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Orders;
 use App\Entity\OrdersStacks;
+use App\Entity\Stacks;
 use App\Repository\OrdersStacksRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,6 +30,7 @@ class OrdersStacksController extends AbstractController
                 'id' => $orderStack->getId(),
                 'order_id' => $orderStack->getOrdId()?->getId(),
                 'stack_id' => $orderStack->getStcId()?->getId(),
+                'stack_title' => $orderStack->getStcId()?->getStcTitle(),
             ];
         }
 
@@ -50,17 +53,17 @@ class OrdersStacksController extends AbstractController
             }
         }
 
+        // Загружаем сущности
+        $order = $entityManager->getRepository(Orders::class)->find($data['order_id']);
+        $stack = $entityManager->getRepository(Stacks::class)->find($data['stack_id']);
+
+        if (!$order || !$stack) {
+            return $this->json(['error' => 'Order or Stack not found'], JsonResponse::HTTP_NOT_FOUND);
+        }
+
         $orderStack = new OrdersStacks();
-
-        // В реальном приложении нужно загрузить сущности Order и Stack
-        // $order = $entityManager->getRepository(Orders::class)->find($data['order_id']);
-        // $stack = $entityManager->getRepository(Stacks::class)->find($data['stack_id']);
-        // $orderStack->setOrdId($order);
-        // $orderStack->setStcId($stack);
-
-        // Временно используем сеттеры с ID
-        $orderStack->setOrdId($data['order_id']);
-        $orderStack->setStcId($data['stack_id']);
+        $orderStack->setOrdId($order);
+        $orderStack->setStcId($stack);
 
         $entityManager->persist($orderStack);
         $entityManager->flush();
@@ -69,40 +72,44 @@ class OrdersStacksController extends AbstractController
             'id' => $orderStack->getId(),
             'order_id' => $orderStack->getOrdId()?->getId(),
             'stack_id' => $orderStack->getStcId()?->getId(),
+            'stack_title' => $orderStack->getStcId()?->getStcTitle(),
         ], JsonResponse::HTTP_CREATED);
     }
 
     /**
-     * Получить связь по ID (GET /api/orders-stacks/{ord_stc_id})
+     * Получить связь по ID (GET /api/orders-stacks/{id})
      */
-    #[Route('/{ord_stc_id}', name: 'orders_stacks_show', methods: ['GET'])]
-    #[ParamConverter('orderStack', options: ['mapping' => ['ord_stc_id' => 'id']])]
+    #[Route('/{id}', name: 'orders_stacks_show', methods: ['GET'])]
     public function show(OrdersStacks $orderStack): JsonResponse
     {
         return $this->json([
             'id' => $orderStack->getId(),
             'order_id' => $orderStack->getOrdId()?->getId(),
             'stack_id' => $orderStack->getStcId()?->getId(),
+            'stack_title' => $orderStack->getStcId()?->getStcTitle(),
         ]);
     }
 
     /**
-     * Обновить связь (PUT /api/orders-stacks/{ord_stc_id})
+     * Обновить связь (PUT /api/orders-stacks/{id})
      */
-    #[Route('/{ord_stc_id}', name: 'orders_stacks_edit', methods: ['PUT', 'PATCH'])]
-    #[ParamConverter('orderStack', options: ['mapping' => ['ord_stc_id' => 'id']])]
+    #[Route('/{id}', name: 'orders_stacks_edit', methods: ['PUT', 'PATCH'])]
     public function edit(Request $request, OrdersStacks $orderStack, EntityManagerInterface $entityManager): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
         if (isset($data['order_id'])) {
-            // Загрузить сущность Order и установить
-            $orderStack->setOrdId($data['order_id']);
+            $order = $entityManager->getRepository(Orders::class)->find($data['order_id']);
+            if ($order) {
+                $orderStack->setOrdId($order);
+            }
         }
 
         if (isset($data['stack_id'])) {
-            // Загрузить сущность Stack и установить
-            $orderStack->setStcId($data['stack_id']);
+            $stack = $entityManager->getRepository(Stacks::class)->find($data['stack_id']);
+            if ($stack) {
+                $orderStack->setStcId($stack);
+            }
         }
 
         $entityManager->flush();
@@ -111,14 +118,14 @@ class OrdersStacksController extends AbstractController
             'id' => $orderStack->getId(),
             'order_id' => $orderStack->getOrdId()?->getId(),
             'stack_id' => $orderStack->getStcId()?->getId(),
+            'stack_title' => $orderStack->getStcId()?->getStcTitle(),
         ]);
     }
 
     /**
-     * Удалить связь (DELETE /api/orders-stacks/{ord_stc_id})
+     * Удалить связь (DELETE /api/orders-stacks/{id})
      */
-    #[Route('/{ord_stc_id}', name: 'orders_stacks_delete', methods: ['DELETE'])]
-    #[ParamConverter('orderStack', options: ['mapping' => ['ord_stc_id' => 'id']])]
+    #[Route('/{id}', name: 'orders_stacks_delete', methods: ['DELETE'])]
     public function delete(OrdersStacks $orderStack, EntityManagerInterface $entityManager): JsonResponse
     {
         $entityManager->remove($orderStack);
